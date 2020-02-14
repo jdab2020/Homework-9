95b4db1f3390ed037405d53bc65c86217a7ac6ce
jdab2020 <jdabalos90@gmail.com>
2020-02-14 12:29:17 -0800

    added writing part

---
+++ Develop/README.md
@@ -0,0 +1,36 @@
+<h1>Homework 9</h1>
+
+<h2>Project Description: </h2>
+
+<h3>Generate a new readme file</h3>
+
+<h2>Table of Contents: </h2>
+
+<h3>Installation + Usage + License + Contributing + Tests + Questions</h3>
+
+<h2>Installation: </h2>
+
+<h3>npm install</h3
+
+<h2>Usage: </h2>
+
+<h3>Input information and return a new readme file</h3>
+
+<h2>License: </h2>
+
+<h3>MIT</h3>
+
+<h2>Contributing: </h2>
+
+<h3>Github</h3>
+
+<h2>Testing: </h2>
+
+<h3>npm test
+
+<img width="30" src="https://avatars2.githubusercontent.com/u/59621364?v=4"><img>
+
+<h2>Questions</h2>
+
+<h3>If you have any questions please contact me at <a href="https://api.github.com/users/jdab2020">https://api.github.com/users/jdab2020</a></h3>
+
--- Develop/index.js
+++ Develop/index.js
@@ -62,29 +62,34 @@ const questions = [
 ]
 
 function init() {
+    var data = "";
+    
     inquirer
         .prompt(questions)
         .then(function (input) {
+            data += `<h1>${input.project_title}</h1>\n\n`;
+            data += `<h2>Project Description: </h2>\n\n<h3>${input.project_description}</h3>\n\n`;
+            data += `<h2>Table of Contents: </h2>\n\n<h3>${input.table_of_contents}</h3>\n\n`;
+            data += `<h2>Installation: </h2>\n\n<h3>${input.installation}</h3\n\n`;
+            data += `<h2>Usage: </h2>\n\n<h3>${input.usage}</h3>\n\n`;
+            data += `<h2>License: </h2>\n\n<h3>${input.stack}</h3>\n\n`;
+            data += `<h2>Contributing: </h2>\n\n<h3>${input.user_contributions}</h3>\n\n`;
+            data += `<h2>Testing: </h2>\n\n<h3>${input.tests}\n\n`;
+            
+            console.log(JSON.stringify(input));
+            // console.log(data);
             const userName = input.username;
             const queryUrl = `https://api.github.com/users/${userName}/repos?per_page=100`;
-
+            
             axios
                 .get(queryUrl)
                 .then(function (response) {
-                    data = {
-                        pojectTitle: input.project_title,
-                        projectDescription: input.project_description,
-                        tableOfContents: input.table_of_contents,
-                        installationGuide: input.installation,
-                        repoUsage: input.usage,
-                        license: input.stack,
-                        contributions: input.user_contributions,
-                        testing: input.tests,
-                        image: response.data[0].owner.avatar_url,
-                        contact: response.data[0].owner.url
-                    }
+                    data += `<img width="30" src="${response.data[0].owner.avatar_url}"><img>\n\n`
+                    data += `<h2>Questions</h2>\n\n<h3>If you have any questions please contact me at <a href="${response.data[0].owner.url}">${response.data[0].owner.url}</a></h3>\n\n`
                     console.log(data);
-                    // writeToFile("README.md",data);
+                    writeFileAsync("README.md",data).then(function(){
+                        console.log("Success");
+                    });
 
                 })
 
